f676240c344c8a44d1f91dd403e4c2fbb9c74acf
benni3005 <benni3005+github-com@gmail.com>
2016-10-11 16:35:26 +0200

    Second commit

--- src/detail.js
+++ src/detail.js
@@ -1,4 +1,28 @@
+import {inject} from 'aurelia-framework';
+import {BackendService} from './services/backend-service';
+
+@inject(BackendService)
 export class Detail {
 
+  constructor(backendService) {
+    this.backendService = backendService;
+
+    this.title = '';
+    this.description = '';
+    this.createdDate = null;
+    this.updatedDate = null;
+    this.priority = null;
+  }
+
+  activate(routeConfig, params) {
+    this.backendService.getCard(routeConfig.id)
+      .then((card) => {
+        this.title = card.title;
+        this.description = card.description;
+        this.createdDate = card.createdDate;
+        this.updatedDate = card.updatedDate;
+        this.priority = card.priority;
+      });
+  }
 
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export class Index {
     config.title = 'My Router Title';
     config.map([
       { name: 'app', title: 'Home', moduleId: 'app', route: ['', '/app'], nav: true },
-      { name: 'detail', title: 'Detail', moduleId: 'detail', route: ['detail'], nav: true }
+      { name: 'detail', title: 'Detail', moduleId: 'detail', route: ['detail/:id'], nav: false }
     ]);
 
     this.router = router;
